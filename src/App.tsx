f0fe163646c872be6465b038c74a6d38702315d1
richard-nagy <big.ricsoo@gmail.com>
2025-11-25 10:52:02 +0100

    Remove sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { AppSidebar } from "@/components/AppSidebar";
 import { SettingsDropDown } from "@/components/SettingsDropdown";
 import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import { ThemeToggle } from "@/components/theme/ThemeToggle";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { Toaster } from "@/components/ui/sonner";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { BookSearchProvider } from "./components/book/BookSearchProvider";
+import { Button } from "./components/ui/button";
 
 const App = () => {
     return (
@@ -15,10 +15,13 @@ const App = () => {
                 <Toaster />
                 <SidebarProvider>
                     <div className="flex w-full h-screen">
-                        <AppSidebar />
+                        {/* <AppSidebar /> */}
                         <main className="flex-1 overflow-y-auto flex flex-col">
                             <div className="flex justify-between w-full sticky top-0 p-3">
-                                <SidebarTrigger />
+                                {/* <SidebarTrigger /> */}
+                                <Button asChild>
+                                    <Link to="/">Go to Home Page</Link>
+                                </Button>
                                 <div className="flex gap-3">
                                     <ThemeToggle />
                                     <SettingsDropDown />
