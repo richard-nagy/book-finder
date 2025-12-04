75045498682683dcd8258ecb2d5ca49a6fdca728
richard-nagy <big.ricsoo@gmail.com>
2025-12-04 11:31:57 +0100

    Add checker to show errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,16 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        checker({
+            typescript: true,
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
