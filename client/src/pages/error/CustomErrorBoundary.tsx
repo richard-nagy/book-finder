cd62ee615c16e229e56b698f8c713994a9ba3a53
richard-nagy <big.ricsoo@gmail.com>
2025-12-18 02:03:26 +0100

    Client: Fix error pages

--- client/src/pages/error/CustomErrorBoundary.tsx
+++ client/src/pages/error/CustomErrorBoundary.tsx
@@ -1,36 +1,33 @@
 import { Button } from "@/components/ui/button";
-import { useBook } from "@/context/BookContext";
 import { Page } from "@/lib/types";
 import { HomeIcon } from "lucide-react";
 import type { FC } from "react";
 import { useNavigate, useRouteError } from "react-router-dom";
+import { Typography } from "@/components/ui/typography.tsx";
 
 const CustomErrorBoundary: FC = () => {
-    const { clearResults } = useBook();
     const navigate = useNavigate();
     const error = useRouteError();
     const isError = error instanceof Error;
 
-    console.error("Route Error Caught:", error);
-    clearResults();
-
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen p-6">
-            <h1 className="text-3xl font-bold pb-6">
-                Oops! Something went wrong.
-            </h1>
-            <p className="text-lg">
+        <div className="flex flex-col gap-2 items-center justify-center min-h-screen p-6">
+            <Typography variant="h2">Oops! Something went wrong.</Typography>
+            <Typography variant="p">
                 We encountered an unexpected error while loading this page.
-            </p>
+            </Typography>
             {isError && (
                 <div className="text-sm overflow-auto">
-                    <h2 className="font-mono font-semibold mb-2">
+                    <Typography
+                        variant="h4"
+                        className="font-mono font-semibold mb-2"
+                    >
                         Error Details:
-                    </h2>
-                    <p className="whitespace-pre-wrap">{error.message}</p>
+                    </Typography>
+                    <Typography variant="muted">{error.message}</Typography>
                 </div>
             )}
-            <Button asChild onClick={() => navigate(Page.homepage)}>
+            <Button className="mt-4" onClick={() => navigate(Page.homepage)}>
                 <HomeIcon /> Go Home
             </Button>
         </div>
--- client/src/pages/error/PageNotFound.tsx
+++ client/src/pages/error/PageNotFound.tsx
@@ -1,22 +1,18 @@
 import { Button } from "@/components/ui/button";
-import { useBook } from "@/context/BookContext";
 import { Page } from "@/lib/types";
 import { HomeIcon } from "lucide-react";
 import { type FC, type ReactElement } from "react";
 import { useNavigate } from "react-router-dom";
+import { Typography } from "@/components/ui/typography.tsx";
 
 const PageNotFound: FC = (): ReactElement => {
-    const { clearResults } = useBook();
-
     const navigate = useNavigate();
 
-    clearResults();
-
     return (
-        <div className="text-center p-12">
-            <h2 className="text-6xl font-bold mb-4">404</h2>
-            <p className="text-2xl mb-6">Page Not Found</p>
-            <Button asChild onClick={() => navigate(Page.homepage)}>
+        <div className="flex flex-col gap-2 items-center justify-center min-h-full">
+            <Typography variant="h1"> 404</Typography>
+            <Typography variant="h4">Page Not Found</Typography>
+            <Button className="mt-2" onClick={() => navigate(`/${Page.homepage}`)}>
                 <HomeIcon /> Go Home
             </Button>
         </div>
