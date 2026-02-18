0bc02182fad76d59ffe170422c98744907b8020a
richard-nagy <big.ricsoo@gmail.com>
2026-02-18 13:44:09 +0100

    Client: Add vite-plugin-static-copy to rename index.html to 404.html during build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     base: "/book-finder/",
@@ -17,6 +18,16 @@ export default defineConfig({
                     'eslint "./src/**/*.{ts,tsx,js,jsx}" --max-warnings=0',
             },
             overlay: { initialIsOpen: true },
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "index.html",
+                    dest: "",
+                    rename: "404.html",
+                },
+            ],
+            hook: "writeBundle",
         }),
     ],
     resolve: {
